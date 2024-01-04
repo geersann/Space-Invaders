944c6ce6a17d29112096630578f45dcbf3a2804d
Bohdan Kavyn <b.kavyn2000@gmail.com>
2024-01-04 04:39:25 +0200

    change index js + add img

--- index.js
+++ index.js
@@ -60,7 +60,125 @@ class Player {
         }
     }
 }
+
+class Projectile {
+    constructor({position, velocity}){
+        this.position = position
+        this.velocity = velocity
+
+        this.radius = 3
+    }
+    draw() {
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, 
+            Math.PI * 2)
+        c.fillStyle = "red"
+        c.fill()
+        c.closePath()
+    }
+
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+}
+
+class Invader {
+    constructor({position}) {
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+        const image = new Image()
+        image.src = "./img/invader.png"
+        image.onload = () => {
+            const scale = 1
+            this.image = image
+            this.width = image.width * scale
+            this.height = image.height * scale 
+            this.position = {
+                x: position.x,
+                y: position.y
+            }
+        }
+    }
+
+    draw() {
+        // c.fillStyle = "red"
+        // c.fillRect(this.position.x, this.position.y, this.width,
+        //      this.height)
+
+        c.drawImage(
+            this.image,
+            this.position.x, 
+            this.position.y,
+            this.width, 
+            this.height
+        )
+    }
+    update({velocity}) {
+        if (this.image) {
+            this.draw()
+            this.position.x += velocity.x
+            this.position.y += velocity.y
+        }
+    }
+}
+
+class Grid {
+    constructor() {
+        this.position = {
+            x: 0,
+            y: 0
+        }
+
+        this.velocity = {
+            x: 3,
+            y: 0
+        }
+
+        this.invaders = [];
+
+        const columns = Math.floor(Math.random() * 10 + 5);
+        const rows = Math.floor(Math.random() * 5 + 2);
+
+        this.width = columns * 30
+
+        for(let x = 0; x < columns; x++) {
+            for(let y = 0; y < rows; y++) {
+                this.invaders.push(
+                        new Invader({
+                            position: {
+                                x: x * 30,
+                                y: y * 30
+                            }
+                        })
+                    )
+                }
+        }    
+        console.log(this.invaders)
+    }
+
+    update() {
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+
+        this.velocity.y = 0
+
+        if(this.position.x + this.width >= canvas.width || this.
+            position.x <= 0) {
+            this.velocity.x = -this.velocity.x
+            this.velocity.y = 30
+        }
+    }
+}
+
 const player = new Player()
+const Projectiles = [];
+const grids = [new Grid()]
+
 const keys = {
     a: {
         pressed: false
@@ -78,6 +196,23 @@ function animate() {
     c.fillStyle = "black"
     c.fillRect(0, 0, canvas.width, canvas.height)
     player.update();
+    Projectiles.forEach((Projectile, index) => {
+
+        if(Projectile.position.y + Projectile.radius <= 0){
+            setTimeout(() => {
+                Projectiles.splice(index, 1)
+            }, 0)
+        }else{
+        Projectile.update()
+        }
+    })
+
+    grids.forEach((grid) => {
+        grid.update()
+        grid.invaders.forEach(invader => {
+            invader.update({velocity: grid.velocity})
+        })
+    })
 
     if (keys.a.pressed && player.position.x >= 0) {
         player.velocity.x = -7
@@ -97,16 +232,29 @@ animate();
 addEventListener("keydown", ({key}) => {
     switch (key) {
         case "a":
-            console.log("left")
+            // console.log("left")
             
             keys.a.pressed = true
             break;
         case "d":
-            console.log("right")
+            // console.log("right")
             keys.d.pressed = true
             break; 
         case " ":
-            console.log("space")
+            // console.log("space")
+            Projectiles.push(
+                    new Projectile({
+                position: {
+                    x: player.position.x + player.width / 2,
+                    y: player.position.y
+                },
+                velocity: {
+                    x: 0,
+                    y: -10
+                }  
+                })
+            )
+            // console.log(Projectiles)
             break; 
     }
 })
@@ -114,16 +262,16 @@ addEventListener("keydown", ({key}) => {
 addEventListener("keyup", ({key}) => {
     switch (key) {
         case "a":
-            console.log("left")
+            // console.log("left")
         
             keys.a.pressed = false;
             break;
         case "d":
-            console.log("right")
+            // console.log("right")
             keys.d.pressed = false;
             break; 
         case " ":
-            console.log("space")
+            // console.log("space")
             break; 
     }
 })
