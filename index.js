bad3b5f0ca699bb1e63fd50c759aa30ca3b602c6
geersann <148795588+geersann@users.noreply.github.com>
2024-01-26 03:42:07 +0000

    re:edit index.js

--- index.js
+++ index.js
@@ -13,27 +13,8 @@ export const overTitle = document.querySelector(".over-title");
 export const menuButton = document.querySelector(".menu-button");
 export const canvas = document.querySelector("canvas");
 export const c = canvas.getContext("2d")
-
 canvas.width = 1500
 canvas.height = 867
-
-export const soundsEffect = {
-    playerShootSound: new Audio("./sounds/shoot.wav"),
-    explodeSound: new Audio("./sounds/explode.wav"),
-    enemyShootSound: new Audio("./sounds/enemyShoot.wav"),
-    gameOverSound: new Audio("./sounds/gameOver.mp3"),
-    gameStartSound: new Audio("./sounds/start.mp3"),
-    selectSound: new Audio("./sounds/select.mp3"),
-    backgroundSound: new Audio("./sounds/backgroundMusic.wav")
-};
-
-soundsEffect.playerShootSound.volume = 0.1;
-soundsEffect.explodeSound.volume = 0.3;
-soundsEffect.enemyShootSound.volume = 0.2;
-soundsEffect.gameOverSound.volume = 0.1;
-soundsEffect.gameStartSound.volume = 0.1;
-soundsEffect.selectSound.volume = 0.1;
-soundsEffect.backgroundSound.volume = 0.5;
 
 export const startButton = new StartButton();
 export const isActive = (obj) => obj.isActive;
@@ -64,6 +45,24 @@ export let game = {
     over: true,
     active: true,
 }
+
+export const soundsEffect = {
+    playerShootSound: new Audio("./sounds/shoot.wav"),
+    explodeSound: new Audio("./sounds/explode.wav"),
+    enemyShootSound: new Audio("./sounds/enemyShoot.wav"),
+    gameOverSound: new Audio("./sounds/gameOver.mp3"),
+    gameStartSound: new Audio("./sounds/start.mp3"),
+    selectSound: new Audio("./sounds/select.mp3"),
+    backgroundSound: new Audio("./sounds/backgroundMusic.wav")
+};
+
+soundsEffect.playerShootSound.volume = 0.1;
+soundsEffect.explodeSound.volume = 0.3;
+soundsEffect.enemyShootSound.volume = 0.2;
+soundsEffect.gameOverSound.volume = 0.1;
+soundsEffect.gameStartSound.volume = 0.1;
+soundsEffect.selectSound.volume = 0.1;
+soundsEffect.backgroundSound.volume = 0.5;
 
 for (let i = 0; i < 100; i++) {
     particles.push(new Particle({
