d49d7fdbf3804a999a715dfd4074e9c94400836a
geersann <148795588+geersann@users.noreply.github.com>
2024-01-31 02:33:30 +0000

    delete js file

--- modules/animationModule.js
+++ modules/animationModule.js
@@ -372,7 +372,7 @@ export function animate() {
     spawnBoss = false;
     }
 
-    if (spawnBoss && score >= 1000) {
+    if (spawnBoss && score >= 100000) {
         updateHealthBar();
         soundsEffect.backgroundSound.volume = 0;
         soundsEffect.bossStartFight.play();
